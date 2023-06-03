f939b780148018aca4fe874a8603a60cafda6361
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-06-04 00:28:06 +0900

    新規作成

---
+++ src/to/msn/wings/selflearn/chap02/Practice5.java
@@ -0,0 +1,24 @@
+package to.msn.wings.selflearn.chap02;
+
+public class Practice5 {
+  public static void main(String[] args) {
+    // ①
+    var value=10D;
+
+    // ②
+    System.out.println("ようこそ、\nJavaの世界へ！");
+
+    // ③
+    String str =null;
+
+    // ④
+    int[][] data = new int[5][4];
+
+    // ⑤
+    var list = new int[][]{
+      {2,3,5},
+      {1,2},
+      {10,11,12,13},
+    };
+  }
+}
